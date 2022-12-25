5e77b97bac57238605b3caf219700d41a325a5fe
ZinovevSasha <109625848+ZinovevSasha@users.noreply.github.com>
2022-12-25 12:27:58 +0200

    add launchScreen image and assets

---
+++ ImageList/Helper/UIColor+Extension.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+extension UIColor {
+    static var gradientStart: UIColor { UIColor(named: "gradientStart")! }
+    static var gradientStop: UIColor { UIColor(named: "gradientStop")! }
+    static var ypWhite50: UIColor { UIColor(named: "ypWhite50")! }
+    static var ypShadow: UIColor { UIColor(named: "ypShadow")! }
+    static var ypBackground: UIColor { UIColor(named: "ypBackground")! }
+    static var ypBlack: UIColor { UIColor(named: "ypBlack")! }
+    static var ypBlue: UIColor { UIColor(named: "ypBlue")! }
+    static var ypGray: UIColor { UIColor(named: "ypGray")! }
+    static var ypRed: UIColor { UIColor(named: "ypRed")! }
+    static var ypWhite: UIColor { UIColor(named: "ypWhite")! }
+}
---
+++ ImageList/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ImageList
+//
+//  Created by Александр Зиновьев on 25.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .ypBlack
+    }
+}
+
